7a11459e9d0a622fb2d9f55692d5165b0d8452cb
Jorge Blandon <jackronaldo@hotmail.com>
2022-02-27 15:50:54 -0500

    Se termina  lo ejericicios con el concepto de  Colas

--- Actividad_03/ColasMain.java
+++ Actividad_03/ColasMain.java
@@ -7,7 +7,8 @@ import javax.naming.LinkException;
 /**
  * ColasMain
  * 
- * En este Ejemplo se utiliza la interface Queue, perteneciste al Framework Collection en Java
+ * En este Ejemplo se utiliza la interface Queue, 
+ * perteneciste al Framework Collection en Java
  */
 public class ColasMain {
 
@@ -26,12 +27,25 @@ public class ColasMain {
 
            //Contar los elementos de una Cola
            
-           System.out.println("El tamaño de la Cola: "+list01.size());
+           System.out.println("-> El tamaño de la Cola: "+list01.size());
 
            //Saber si una Cola esta vacia
-           System.out.println("Esta la Cola vacia? "+list01.isEmpty());
+           System.out.println("-> Esta la Cola vacia? :  "+list01.isEmpty());
 
-           //
+           //Acceder a un elemento de la Cola
+           list01.add(154);
+
+           System.out.println("-> Lista con elemento agregado: ");
+           list01.stream().forEach((x)-> System.out.print(x +"| "));
+           System.out.println(" ");
+
+           //Borrar todos los elementos de la cola
+           System.out.println("-> Removiendo todos lo elementos de la Cola:");
+           list01.clear();
+           System.out.println("-> Esta la Cola vacia? :  "+list01.isEmpty());
+
+
+
                 
     }
 
