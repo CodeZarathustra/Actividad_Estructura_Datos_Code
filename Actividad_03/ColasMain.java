fd582bd911906b7ee87292d151cdf308f1bdcb67
Jorge Blandon <jackronaldo@hotmail.com>
2022-02-27 13:18:15 -0500

    Se añaden los ejercicios de la actividad 03

---
+++ Actividad_03/ColasMain.java
@@ -0,0 +1,41 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+
+import javax.naming.LinkException;
+
+/**
+ * ColasMain
+ * 
+ * En este Ejemplo se utiliza la interface Queue, perteneciste al Framework Collection en Java
+ */
+public class ColasMain {
+
+    public static void main(String[] args) {
+        
+        //Se crea una colección de tipo cola sobre la que trabajar
+        Queue<Integer> list01= new LinkedList<>();
+        
+            //Se llena la cola con 50 numeros
+            for (int i = 0; i < 20; i++) {
+                list01.add(i);
+                
+            }
+            list01.stream().forEach((x)-> System.out.print(x +"| "));
+            System.out.println(" ");
+
+           //Contar los elementos de una Cola
+           
+           System.out.println("El tamaño de la Cola: "+list01.size());
+
+           //Saber si una Cola esta vacia
+           System.out.println("Esta la Cola vacia? "+list01.isEmpty());
+
+           //
+                
+    }
+
+      
+        
+    
+}
